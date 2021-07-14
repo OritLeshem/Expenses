4d5503c237b0d65db9c8f62b06af12a909841283
OritLeshem <azmono@gmail.com>
2021-07-14 17:43:28 -0400

    css changes

---
+++ src/components/Ex/Expenses/Expenses.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import Card from "../../UI/Card/Card";
+import "./Expenses.css";
+import ExpensesFilter from "../ExpensesFilter/ExpensesFilter";
+import ExpensesList from "../ExpensesList/ExpensesList";
+import ExpensesChart from "../ExpensesChart/ExpensesChart";
+
+const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2020");
+  const filterChangrHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+    console.log("Expenses", selectedYear);
+  };
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangrHandler}
+        />
+        <ExpensesChart expenses={filteredExpenses} />
+        <ExpensesList items={filteredExpenses} />
+      </Card>
+    </div>
+  );
+};
+export default Expenses;
